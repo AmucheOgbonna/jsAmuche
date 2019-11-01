76732da87dcdebac893957a127fb7aac0e0dff9b
AmucheOgbonna <amuche247cynthia@gmail.com>
2019-11-01 15:01:36 +0100

    Today's commit

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+//Displays name.
+document.write("<strong>My name is Amuche</strong>");
+
+//Displays name.
+document.write("<strong>Ogbonna</strong>");
+
+//Displays name.
+document.write("<strong>Cynthia. </strong>");
+
+let firstName = 'Clara';
+console.log(firstName);
+firstName= 'cynthia';
+console.log(firstName);
+let description= "The name of my Aunt is Bisola";
+let address= "She lives in Calabar";
+let age = "She is eight years old";
+console.log(description,address, age);
+description= "My name is bisola, I live at No " + 12 + " laggon street.";
+document.write(description);
+
+let sum = (3*10)+(20/5);
+document.write(sum);
